5b4ecc0baef5819a2583ecb1c01cbe5a6d8082cd
Naomi Nyongesa <naomi.nyongesa@student.moringaschool.com>
2022-06-02 21:51:44 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var employee = {};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    
+
+    var newEmployee = Object.assign({}, employee, {[key]: value});
+    return newEmployee;
+}
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    var clone = Object.assign({}, employee);
+    delete clone[key];
+    return clone;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
